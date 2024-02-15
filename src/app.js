8df28b3669cda8645c3c9612dd93e44310f075bf
Francoise <84253938+FrancoiseC@users.noreply.github.com>
2024-02-15 01:37:14 +0000

    generator finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed on", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  document.getElementById("generate-btn").addEventListener("click", function() {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+  });
 };
